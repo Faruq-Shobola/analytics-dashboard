a70bf77f685f1a538e8aa0efa5937fa936dc6b7e
Shobola Faruq <66446091+Faruq-Shobola@users.noreply.github.com>
2024-01-27 07:28:34 +0000

    Fix mobile sidebar

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Main from './component/Main'
 import Navbar from './component/Navbar'
-// import Sidebar from './component/Sidebar'
+import Sidebar from './component/Sidebar'
 
 const Dashboard = () => {
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <>
       <div className="">
-        {/* <aside className="hidden md:block fixed overflow-auto h-screen left-0 top-0 w-20 py-5 bg-slate-100">
-          <Sidebar />
-        </aside> */}
-        <div className='ml-0 md:!ml-20'>
-            <nav className="hidden md:block top-0 left-0 sticky z-10">
-            <Navbar />
+        {/* {isSidebarOpen && */}
+          <aside className={`fixed z-50 h-screen top-0 py-5 bg-slate-100 transition-all duration-500 ${isSidebarOpen ? 'left-0' : '-left-full md:left-0'}`}>
+           <Sidebar />
+           <div className="absolute top-5 left-24 z-100 rounded border border-black bg-gray-100 p-2 flex justify-center items-center">
+              <button className="text-gray-800 transition hover:text-gray-800/75 btn-ghost" onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+          </div>
+          </aside> 
+        {/* } */}
+          <div className='ml-0 md:!ml-20'>
+            <nav className="top-0 left-0 sticky z-10">
+            <Navbar toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
             </nav>
             <main className="bg-gray-50 p-4">
                 <Main />
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import { Search, Person1, ArrowDown, Bell, Calender } from "../assets";
 
-const Navbar = () => {
+const Navbar = ({ toggleSidebar }) => {
   return (
     <div className="bg-slate-50 grid grid-cols-10 p-3 items-center">
-      <div className="col-span-6 flex justify-between items-center px-3">
-        <a className="font-medium text-2xl capitalize" href="/">
+      <div className="col-span-5 md:col-span-6 flex justify-start md:justify-between items-center px-3 gap-3">
+        <div className="block md:hidden">
+          <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75" onClick={toggleSidebar}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+        </div>
+        <a className="font-medium text-lg md:text-2xl capitalize" href="/">
           dashboard
         </a>
 
         {/* Search */}
-        <div className="flex items-center gap-3 bg-white border border-gray-200 rounded-full px-6 py-3">
+        <div className="hidden md:flex items-center gap-3 bg-white border border-gray-200 rounded-full px-6 py-3">
           <img src={Search} alt="Logo" className="w-5 h-5" />
           <input
             type="text"
@@ -20,9 +34,9 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="col-span-4 flex justify-between items-center px-2">
+      <div className="col-span-5 md:col-span-4 flex justify-end md:justify-between items-center gap-3 px-2">
         {/* Calender */}
-        <div className="flex items-center gap-3  px-4 py-2">
+        <div className="hidden md:flex items-center gap-3  px-4 py-2">
           <img src={Calender} alt="Logo" className="w-5 h-5" />
           <p className="text-base font-semibold">November 15, 2023</p>
         </div>
@@ -37,16 +51,16 @@ const Navbar = () => {
           <span className="badge badge-xs badge-info text-right absolute top-0.5 right-0"></span>
         </div>
         {/* Profile */}
-        <div className="flex items-center gap-4 bg-white border border-gray-200 rounded-full px-4 py-2">
+        <div className="flex items-center gap-2 md:gap-4 bg-white border border-gray-200 rounded-full px-2 md:px-4 py-1 md:py-2">
           <img src={Person1} alt="Logo" className="w-10 h-10 rounded-full" />
-          <div className="text-right space-y-3">
+          <div className="hidden md:block text-right space-y-3">
             <h5 className="font-medium text-lg leading-3">Justin Bergson</h5>
             <p className="text-base text-gray-500 leading-3">
               Justin@gmail.com
             </p>
           </div>
           <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-ghost p-2">
+            <div tabIndex={0} role="button" className="btn btn-ghost p-1 md:p-2">
               <img src={ArrowDown} alt="Logo" className="w-5 h-5" />
             </div>
             <ul
